0dd76b669c1aabb7d77679ff965324f6e9790b59
Vladyslav Vintoniuk <spatiumfabri@gmail.com>
2022-09-20 17:17:06 +0300

    Add several route handlers for CRUD on task

--- controllers/task_controllers.js
+++ controllers/task_controllers.js
@@ -9,10 +9,51 @@ const showAllTasks = async (req, res) => {
         return res.redirect(303, 'sign-in');
     }
 
-    res.type('text/plain');
-    res.send('All tasks' + ' ' + req.cookies.userId);
+    const tasks = await task.getAllTasks(userid)
+
+    res.render('tasks', {tasks: tasks})
+};
+
+const createTask = async (req, res) => {
+    const userid = req.cookies.userId;
+    if (!await auth.isUserActive(userid, req.ip)) {
+        res.clearCookie('userId')
+        return res.redirect(303, 'sign-in');
+    }
+
+    const text = req.body.text;
+
+    await task.createTask(text, userid);
+
+    return res.redirect(303, '/tasks');
+};
+
+const editTask = async (req, res) => {
+    const userid = req.cookies.userId;
+    if (!await auth.isUserActive(userid, req.ip)) {
+        res.clearCookie('userId')
+        return res.redirect(303, 'sign-in');
+    }
+
+};
+
+const deleteTask = async (req, res) => {
+    const userid = req.cookies.userId;
+    if (!await auth.isUserActive(userid, req.ip)) {
+        res.clearCookie('userId')
+        return res.redirect(303, 'sign-in');
+    }
+
+    const id = req.body.id;
+
+    await task.deleteTask(id);
+
+    return res.redirect(303, '/tasks');
 };
 
 module.exports = {
     showAllTasks,
+    createTask,
+    editTask,
+    deleteTask,
 };
--- routers/task_router.js
+++ routers/task_router.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 const {
     showAllTasks,
+    createTask,
+    editTask,
+    deleteTask,
 } = require('../controllers/task_controllers.js');
 
 router.get('/', showAllTasks);
+router.post('/create', createTask);
+router.post('/edit', editTask);
+router.post('/delete', deleteTask);
 
 module.exports = router;
